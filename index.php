c2260817ceeebe480cc718186d8caa01abce366d
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-12-07 03:29:10 +0300

    Edit index page and add product modal with form

--- index.php
+++ index.php
@@ -50,6 +50,120 @@
         <div id="fore-flower"></div>
     </div>
 
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Left Sidebar -->
+            <div class="col-md-2">Left Sidebar</div>
+
+            <!-- Main Content -->
+            <div class="col-md-8">
+                    <div class="text-center">Feature Products</div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <h4>Levis Jeans</h4>
+                        <img src="images/products/men4.png" alt="Levis Jeans" class="img-thumb">
+                        <p class="list-price text-danger">List Price <s>$55.99</s></p>
+                        <p class="price">Our Price: $19.99</p>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>Levis Jeans</h4>
+                        <img src="images/products/men4.png" alt="Levis Jeans" class="img-thumb">
+                        <p class="list-price text-danger">List Price <s>$55.99</s></p>
+                        <p class="price">Our Price: $19.99</p>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>Levis Jeans</h4>
+                        <img src="images/products/men4.png" alt="Levis Jeans" class="img-thumb">
+                        <p class="list-price text-danger">List Price <s>$55.99</s></p>
+                        <p class="price">Our Price: $19.99</p>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>Levis Jeans</h4>
+                        <img src="images/products/men4.png" alt="Levis Jeans" class="img-thumb">
+                        <p class="list-price text-danger">List Price <s>$55.99</s></p>
+                        <p class="price">Our Price: $19.99</p>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>Levis Jeans</h4>
+                        <img src="images/products/men4.png" alt="Levis Jeans" class="img-thumb">
+                        <p class="list-price text-danger">List Price <s>$55.99</s></p>
+                        <p class="price">Our Price: $19.99</p>
+                        <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Right Sidebar -->
+            <div class="col-md-2">Right Sidebar</div>
+        </div>
+
+        <footer class="text-center" id="footer">&copy; Copyright 2018 E-Commerce</footer>
+
+        <!-- Details Modal -->
+        <div class="modal fade details-1" id="details-1" tabindex="-1" role="dialog" aria-labelledby="details-1" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title text-center">Levis Jeans</h4>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="center-block">
+                                        <img src="images/products/men4.png" alt="Levis Jeans" class="details img-responsive">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h4>Details</h4>
+                                    <p>These jeans are amazing! They are straight leg, fit great and look sexy, Get a pair while they last</p>
+                                    <hr>
+                                    <p>Price: $34.99</p>
+                                    <p>Brand: Levis</p>
+                                    <form action="add_cart.php" method="post">
+                                        <div class="form-group">
+                                            <div class="col-xs-3">
+                                                <label for="quantity">Quantity:</label>
+                                                <input type="text" class="form-control" id="quantity" name="quantity">
+                                            </div>
+                                            <div class="col-xs-9"></div>
+                                            <p>Available: 3</p>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="size">Size:</label>
+                                            <select name="size" id="size" class="form-control">
+                                                <option value=""></option>
+                                                <option value="28">28</option>
+                                                <option value="32">32</option>
+                                                <option value="36">36</option>
+                                            </select>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button class="btn btn-warning" type="submit"><span class="glyphicon glyphicon-shopping-cart"></span> Add To Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
     <script>
         jQuery(window).scroll(function() {
             var vscroll = jQuery(this).scrollTop();
